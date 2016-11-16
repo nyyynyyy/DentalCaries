dd641d2710ffb65032757b161bf601c299f9c119
hINSIh <sunjwan@gmail.com>
2016-11-16 17:41:16 +0900

    boom particle

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,11 +7,17 @@ public class Enemy : MonoBehaviour {
     public float _speed = 3f;
     public float _health = 3f;
 
+	[Header("Death Particle")]
+	public ParticleSystem _deathParticle;
+	public float _particleDuation = 2F;
+
     private MoveEnemy _move;
 
     void Awake()
     {
         _move = GetComponent<MoveEnemy>();
+		_deathParticle = Instantiate(_deathParticle).GetComponent<ParticleSystem>();
+		_deathParticle.transform.SetParent(transform);
     }
 
     public void Spawn(Vector3 spawnPoint, float speed, float health)
@@ -20,6 +26,10 @@ public class Enemy : MonoBehaviour {
         _move.SetMyAnlge();
         _health = health;
         _speed = speed;
+
+		StopCoroutine(ComeBackHomeMyParticle());
+		_deathParticle.transform.localPosition = Vector3.zero;
+
         gameObject.SetActive(true);
     }
 
@@ -38,6 +48,19 @@ public class Enemy : MonoBehaviour {
 
 	public void Death() {
 		// health init code here.
+		_deathParticle.transform.SetParent(null);
+		_deathParticle.Play();
+
+		transform.localScale = Vector3.zero;
+
+		StartCoroutine(ComeBackHomeMyParticle());
+		//Destroy(_deathParticle.gameObject, _particleDuration);
+	}
+
+	IEnumerator ComeBackHomeMyParticle() {
+		yield return new WaitForSeconds(_particleDuation);
+		_deathParticle.transform.SetParent(transform);
 		gameObject.SetActive(false);
+		transform.localScale = Vector3.one;
 	}
 }
