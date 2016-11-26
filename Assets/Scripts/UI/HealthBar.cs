c8fdea21d653033bb81590681d83086bd990a471
nyyynyyy <nyyynyyy@naver.com>
2016-11-26 13:53:09 +0900

    HpBar
    IEnumerator HealthBar.instace.ViewUi(maxHp, nowHp, name);

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public static HealthBar instance;
+
+    public GameObject _me;
+    public Image _bar;
+    public Text _target;
+
+    private bool isUsed;
+
+    void Start()
+    {
+        if (instance)
+        {
+            Debug.LogError("Already loaded instance: HealthBar.cs");
+            return;
+        }
+        instance = this;
+    }
+
+    public IEnumerator ViewUi(float maxHp, float nowHp, string name)
+    {
+        if (!isUsed) _me.SetActive(true);
+        _target.text = name + " " + (nowHp / maxHp) * 100;
+        _bar.rectTransform.sizeDelta = new Vector2(nowHp / maxHp * 1000, 100);
+        yield return new WaitForSeconds(3);
+        _me.SetActive(false);
+    }
+}
