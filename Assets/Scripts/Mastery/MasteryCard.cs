7301be2dd46cc3eb9d0ffd6bcd4044c471968196
nyyynyyy <nyyynyyy@naver.com>
2016-12-16 16:54:53 +0900

    Mastery 0.1

---
+++ Assets/Scripts/Mastery/MasteryCard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MasteryCard : MonoBehaviour {
+
+    private bool _existMastery;
+
+    public GameObject _masteryCard;
+
+    public Image _cardArt;
+    public Text _cardName;
+    public Text _cardDescription;
+    public Text _cardLevel;
+    public Text _cardRank;
+
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/Mastery/MasteryManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasteryManager : MonoBehaviour {
+
+    public static MasteryManager instance;
+
+    void Awake()
+    {
+        if (instance)
+        {
+            Debug.LogError("Multi Instance Running.. : MasteryManager");
+        }
+        instance = this;
+    }
+
+    public void Test(int slotNumber)
+    {
+        Debug.Log(slotNumber + " click");
+    }
+}
